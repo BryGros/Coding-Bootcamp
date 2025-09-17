fe734117c865c8f7e55330bd3b53499ce327fce0
bgrosbach <bryan_grosbach@gideontaylor.com>
2025-09-16 22:03:43 -0600

    Post-Session Commit

---
+++ Week-006/Session-1-Tues/module-26-operators-control-flow/loops.js
@@ -0,0 +1,21 @@
+// Loops allow us to repeat actions
+
+// "For loops" are if we know how many times we want to do an action
+// Count one to ten
+
+for (let i = 0; i < 11; i++) {
+  console.log(i + ", hahaha");
+}
+
+// "While loops" are if we want to take a set of actions, but don't know when that will happen
+
+let stillAlive = true;
+while (stillAlive) {
+  // Play the game
+  console.log("did action 1");
+
+  //   50 loop sinto th game
+  console.log("You fell into lava");
+  stillAlive = false;
+}
+console.log("game over");
