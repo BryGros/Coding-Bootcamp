1b44acf74d19cdb74cdbeb275819bda7bec20a3a
bgrosbach <bryan_grosbach@gideontaylor.com>
2025-10-14 22:01:29 -0600

    Post-Class Commit

---
+++ Week-010/Session-1-Tues/module-046-vanilla-js-api-integration-project/Project-files/script.js
@@ -0,0 +1,1 @@
+// i found this on the web: stock market APIs with free endpoints that is easy on authentication. 1- Alpha Vantage, link:Free Stock APIs in JSON & Excel | Alpha Vantage - https://www.alphavantage.co/
