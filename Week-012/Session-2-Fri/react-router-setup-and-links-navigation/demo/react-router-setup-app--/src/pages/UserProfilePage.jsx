fc4f07f4fe335c8131d2fc5e266fd4c54325ade4
BryGuy MacBook <bryan.grosbach@gmail.com>
2025-11-06 10:34:10 -0700

    Push project files from laptop to repository

--- Week-012/Session-2-Fri/react-router-setup-and-links-navigation/demo/react-router-setup-app--/src/pages/UserProfilePage.jsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from "react-router";
-
-export default function UserProfilePage() {
-  //grab the parameters from our address bar /url
-  const params = useParams();
-
-  return <h1>UserProfile ID: {params.userId}</h1>;
-}
